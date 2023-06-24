37bf4a03e9921f19d45acd377407c88a45e65507
gotohiroki <57242780+gotohiroki@users.noreply.github.com>
2023-06-24 12:03:17 +0900

    Update script.js

--- src/script.js
+++ src/script.js
@@ -20,25 +20,25 @@ const scene = new THREE.Scene()
 const textureLoader = new THREE.TextureLoader()
 
 // ドア
-const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
-const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
-const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
-const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const doorColorTexture = textureLoader.load('./textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('./textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('./textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('./textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
 
 // 壁
-const bricksColorTexture = textureLoader.load('/textures/bricks/color.jpg')
-const bricksAmbientOcclusionTexture = textureLoader.load('/textures/bricks/ambientOcclusion.jpg')
-const bricksNormalTexture = textureLoader.load('/textures/bricks/normal.jpg')
-const bricksRoughnessTexture = textureLoader.load('/textures/bricks/roughness.jpg')
+const bricksColorTexture = textureLoader.load('./textures/bricks/color.jpg')
+const bricksAmbientOcclusionTexture = textureLoader.load('./textures/bricks/ambientOcclusion.jpg')
+const bricksNormalTexture = textureLoader.load('./textures/bricks/normal.jpg')
+const bricksRoughnessTexture = textureLoader.load('./textures/bricks/roughness.jpg')
 
 // 床
-const grassColorTexture = textureLoader.load('/textures/grass/color.jpg')
-const grassAmbientOcclusionTexture = textureLoader.load('/textures/grass/ambientOcclusion.jpg')
-const grassNormalTexture = textureLoader.load('/textures/grass/normal.jpg')
-const grassRoughnessTexture = textureLoader.load('/textures/grass/roughness.jpg')
+const grassColorTexture = textureLoader.load('./textures/grass/color.jpg')
+const grassAmbientOcclusionTexture = textureLoader.load('./textures/grass/ambientOcclusion.jpg')
+const grassNormalTexture = textureLoader.load('./textures/grass/normal.jpg')
+const grassRoughnessTexture = textureLoader.load('./textures/grass/roughness.jpg')
 
 // 床のテクスチャーのサイズを設定
 grassColorTexture.repeat.set(8, 8)
